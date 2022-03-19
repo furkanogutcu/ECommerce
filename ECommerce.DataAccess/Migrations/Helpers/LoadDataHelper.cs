008f120f0c593dac23170420d0e00696945b0ace
furkanogutcu <56116207+furkanogutcu@users.noreply.github.com>
2022-03-19 18:26:05 +0300

    Add LoadDataHelper to Migrations

---
+++ ECommerce.DataAccess/Migrations/Helpers/LoadDataHelper.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using ECommerce.Core.Entities;
+using System.Text.Json;
+
+namespace ECommerce.DataAccess.Migrations.Helpers
+{
+    public static class LoadDataHelper
+    {
+        public static List<T> GetEntityListFromJsonFile<T>(string path)
+            where T : class, IEntity, new()
+        {
+            StreamReader reader = new StreamReader(path);
+            string fileText = reader.ReadToEnd();
+            return JsonSerializer.Deserialize<List<T>>(fileText);
+        }
+    }
+}
